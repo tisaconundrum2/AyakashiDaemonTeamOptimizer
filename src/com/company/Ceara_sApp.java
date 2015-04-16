27ad0ea0b9433fdb5c9499f3af8f53740018dfab
Nicholas Finch <ngf4@nau.edu>
2015-04-16 12:14:02 -0700

    add the initial files

---
+++ src/com/company/Ceara_sApp.java
@@ -0,0 +1,66 @@
+package com.company;
+
+/**
+ * Created by User on 4/7/2015.
+ */
+public class Ceara_sApp {
+
+    public static void main(String[] args) {
+        // write your code here
+        Daemons daemons = new Daemons();
+
+        daemons.insertDaemon("Kotetsu", 1810, 1384, 6);
+        daemons.insertDaemon("Ferris", 720, 980, 9);
+        daemons.insertDaemon("Xuan Nu", 823, 823, 12);
+        daemons.insertDaemon("Rat Clan Boss", 1445, 1121, 13);
+        daemons.insertDaemon("Dinnie", 350, 1120, 10);
+        daemons.insertDaemon("Red Angel", 1399, 1399, 14);
+        daemons.insertDaemon("Gorgonella", 3843, 3843, 29);
+        daemons.insertDaemon("Navvy", 875, 630, 12);
+        daemons.insertDaemon("Onibi", 1233, 1233, 9);
+        daemons.insertDaemon("Sylvia Spoon", 822, 822, 8);
+        daemons.insertDaemon("Kiyohime", 1540, 1610, 16);
+        daemons.insertDaemon("Sherry", 1041, 1104, 7);
+        daemons.insertDaemon("Rapunzel's Hair", 1400, 960, 12);
+        daemons.insertDaemon("Brekkie", 1280, 400, 10);
+        daemons.insertDaemon("Lunchie", 980, 980, 10);
+        daemons.insertDaemon("Chak", 305, 305, 3);
+        daemons.insertDaemon("Koropokkur", 260, 335, 3);
+        daemons.insertDaemon("The Puppeteer", 2170, 2170, 20);
+        daemons.insertDaemon("Kay nine", 3426, 2216, 25);
+        daemons.insertDaemon("Thumping Mallet", 1500, 3225, 15);
+        daemons.insertDaemon("Oshichi", 1430, 1400, 15);
+        daemons.insertDaemon("Kasuumi", 1813, 1410, 14);
+        daemons.insertDaemon("Mary night", 2252, 2252, 20);
+        daemons.insertDaemon("Okiku", 2584, 3079, 14);
+        daemons.insertDaemon("Natalia Nyfe", 820, 870, 9);
+        daemons.insertDaemon("Pinnie", 1412, 1155, 14);
+        daemons.insertDaemon("Anne Droid", 1993, 1473, 17);
+        daemons.insertDaemon("Marsh Kappa", 3030, 3030, 19);
+        daemons.insertDaemon("Pearce", 1904, 1594, 16);
+        daemons.insertDaemon("Blue Angel", 1339, 1469, 8);
+        daemons.insertDaemon("Clifford", 2773, 2413, 19);
+        daemons.insertDaemon("Trident", 1920, 1920, 18);
+        daemons.insertDaemon("Qing Long", 1800, 2183, 12);
+        daemons.insertDaemon("Pipe Fox", 744, 829, 6);
+        daemons.insertDaemon("Barbetta", 1856, 1602, 9);
+        daemons.insertDaemon("Rat Clan Henchman", 744, 660, 7);
+        daemons.insertDaemon("Harpy", 987, 987, 9);
+        daemons.insertDaemon("Lilith", 2341, 2163, 12);
+        daemons.insertDaemon("Aborro", 658, 709, 5);
+        daemons.insertDaemon("Ameno Uzume", 918, 817, 6);
+        daemons.insertDaemon("White Angel", 2234, 2922, 10);
+        daemons.insertDaemon("Obligatory Chocolate", 740, 481, 5);
+        daemons.insertDaemon("Darts", 1001, 799, 8);
+        daemons.insertDaemon("Pillow Phantom", 948, 1023, 5);
+        daemons.insertDaemon("Wind Wheasel", 1396, 1115, 6);
+        daemons.insertDaemon("Susumu Yamazaki", 1461, 1048, 9);
+        daemons.insertDaemon("Snow Sprite", 908, 803, 6);
+        daemons.insertDaemon("Kaede", 391, 314, 3);
+        daemons.insertDaemon("Oiwa Yotsuya", 1230, 1590, 15);
+
+        daemons.getOptimizedAtk(110); //returns 1 result with an optimized set of Daemons; Atk
+        daemons.getOptimizedDef(104); //returns results with an optimized set of Daemons; Def
+    }
+}
+
